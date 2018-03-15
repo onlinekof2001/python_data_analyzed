f0811c14d1006d41313fc6c4e2c80f5051e92739
onlinekof2001 <onlinekof2001@aliyun.com>
2018-03-16 04:03:24 +0800

    week home work finally

--- note_for_python_week8.py
+++ note_for_python_week8.py
@@ -1,16 +1,22 @@
 import numpy as np
 import pandas as pd
 from pandas import Series,DataFrame
+import matplotlib.pyplot as plt
 
-data = pd.read_csv('D:/Software/week8/data/tips.csv', float_format = '%.3f%')
+data = pd.read_csv('D:/Software/week8/data/tips.csv')
 tipbytime = data['tip'].groupby(data['time'])
-tipbytime.mean()
-tipbytime.var()
+tipbytime.agg(['mean','var'])
 
 tbillbysex = data.groupby('sex')['total_bill']
 tipbysex = data['tip'].groupby(data['sex'])
 tbillbysex.mean()
 tbillbysex.std()
+
+
+def standard(val):
+    return (val - val.mean())/val.std()
+    
+data.groupby('sex')[['total_bill','tip']].apply(standard).join(data['sex'])
 
 for i in data.index:
     if data.loc[i]['sex'] == 'Female':
@@ -23,9 +29,29 @@ for i in data.index:
 tipbysmoker = data.groupby('smoker')['tip']
 
 tipbysmoker.mean()[0]
-tipbysmoker.sum()
-round(data['tip'].sum(),2)
+tipbysmoker.sum()),2)
 
 data['tip_pct'] = data['tip'] / data['total_bill']
+smkavg=data.groupby('smoker')['tip_pct'].mean()
+round((smkavg[0] - smkavg[1]),4)
+        
+def stdrd(val):
+    return val.mean(), val.std()
+    
+data.groupby(['sex','size'])['tip_pct'].apply(stdrd)
 
-data.groupby('smoker')['tip_pct'].mean()
+data.groupby(['sex','size'])['tip_pct'].agg(['mean','std'])
+round(data['tip'].sum(
+
+def ts(val):
+    return val.values
+tbillbyts = data.groupby(['time','size'])['total_bill'].apply(ts)
+
+xlim(8,8)
+ylim(8,8)
+for i in np.arange(tbillbyts.count()):
+    if len(tbillbyts.values[i]) != 1:
+        j = i + 1
+        plt.subplot(3,4,j)
+        plt.pie(tbillbyts.values[i], autopct='%1.1f%%')
+    
