b4281d8f8a901860caab7e8d2eb40f1147c506b6
onlinekof2001 <onlinekof2001@aliyun.com>
2018-03-15 15:21:49 +0800

    home_work

--- note_for_python_week8.py
+++ note_for_python_week8.py
@@ -1,4 +1,10 @@
 import numpy as np
 import pandas as pd
-from pandas import Series,Dataframe
+from pandas import Series,DataFrame
 
+data = pd.read_csv('D:/Software/week8/data/tips.csv')
+tipbytime = data['tip'].groupby(data['time'])
+tipbytime.mean()
+tipbytime.var()
+
+tbillbysex = data['total_bill'].groupby()
