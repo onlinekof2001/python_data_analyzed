82f78be48e0108ba48aadf243ecebc348bafbffe
onlinekof2001 <onlinekof2001@aliyun.com>
2019-03-06 01:12:43 +0800

    web app

--- PWP(Python Web Project)/Web_Page.py
+++ PWP(Python Web Project)/Web_Page.py
@@ -1,3 +1,4 @@
+#https://blog.csdn.net/zhizunyu2009/article/details/55510548
 # Django to generate html web page
 
 # Creat a project
@@ -6,4 +7,6 @@ django-admin startproject Web_html
 # Creat a app
 python manage.py startapp Web_app
 
-
+# Startup Web_app
+cd Web_app 
+python manage.py runserver
